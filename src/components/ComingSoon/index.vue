862a374791acf4632e7da08b13cfc3be41ba059c
lanshenzz <495102181@qq.com>
2019-09-24 12:31:34 +0800

    add getCity

--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -1,5 +1,7 @@
 <template>
 <div class="movie_body">
+	 <Loading v-if="isLoading"/>
+	<Scroller v-else>
 				<ul>
 					<!-- <li>
 						<div class="pic_show"><img src="/images/movie_1.jpg"></div>
@@ -26,6 +28,7 @@
 						</div>
 					</li> 
 				</ul>
+	</Scroller>
 				
 </div>
   
@@ -37,13 +40,24 @@ export default {
 	data(){
 		return {
 			comingList:[],
+			isLoading:true,
+			preCityId:-1,
 		}
 	},
-	mounted(){
-		this.axios.get('/api//movieComingList?cityId=10').then((res)=>{
+	activated(){
+		
+		var cityId=this.$store.state.city.id;
+        if(this.preCityId===cityId){
+            return ;
+
+        }
+        this.isLoading=true;
+		this.axios.get('/api//movieComingList?cityId='+cityId).then((res)=>{
 			var msg=res.data.msg;
 			if(msg==='ok'){
 				this.comingList=res.data.data.comingList;
+				this.isLoading=false;
+				this.preCityId=cityId;
 			}
 		});
 	}
